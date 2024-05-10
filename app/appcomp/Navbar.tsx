4cfb50c7bad598de1da7b800868bf44fd77f6a54
nitesh0011 <patilnitesh590@gmail.com>
2024-05-10 22:40:17 +0530

    Add new feature X

--- app/appcomp/Navbar.tsx
+++ app/appcomp/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
             <div className="navbar bg-base-100  shadow-md bg-gradient-to-r from-blue-100 to-green-100">
                 <div className="flex items-center justify-around">
                     <div className="w-16 h-16 flex items-center justify-center rounded-full">
-                        <Image alt="Summarify Icon" src="/summarify.png" width="70" height="70" className="object-cover" />
+                        <Image alt="Summarify Icon" src="/public/Summarify.png" width="70" height="70" className="object-cover" />
                     </div>
                 </div>
                 <div className="flex-none ml-auto">
