186ef2eed0a921f222c3cd977366339dd0f34d77
nitesh0011 <patilnitesh590@gmail.com>
2024-05-10 23:23:21 +0530

    Add new feature X

--- app/appcomp/Navbar.tsx
+++ app/appcomp/Navbar.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
-
+import summarify from '@/public/Summarify.png'
 const Navbar = () => {
     return (
         <div>
             <div className="navbar bg-base-100  shadow-md bg-gradient-to-r from-blue-100 to-green-100 ">
                 <div className="flex items-center justify-around">
                     <div className="w-16 h-16 flex items-center justify-center rounded-full">
-                        <Image alt="Summarify Icon" src="/summarify.png" width="70" height="70" className="object-cover" />
+                        <Image alt="Summarify Icon" src={summarify} width="70" height="70" className="object-cover" />
                     </div>
                 </div>
                 
